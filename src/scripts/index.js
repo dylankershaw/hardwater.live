90cbf12834505629cdd04992ede691ac07c51290
Dylan Kershaw <dylankershaw@me.com>
2020-04-25 00:54:21 -0400

    make sides clickable

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,27 +1,29 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var raycaster = new THREE.Raycaster();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const raycaster = new THREE.Raycaster();
 
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.CubeGeometry(2, 2, 2);
-var cubeMaterials = [
-  new THREE.MeshBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0.95, side: THREE.DoubleSide }),
-  new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true, opacity: 0.95, side: THREE.DoubleSide }),
-  new THREE.MeshBasicMaterial({ color: 0x0000ff, transparent: true, opacity: 0.95, side: THREE.DoubleSide }),
-  new THREE.MeshBasicMaterial({ color: 0xffff00, transparent: true, opacity: 0.95, side: THREE.DoubleSide }),
-  new THREE.MeshBasicMaterial({ color: 0xff00ff, transparent: true, opacity: 0.95, side: THREE.DoubleSide }),
-  new THREE.MeshBasicMaterial({ color: 0x00ffff, transparent: true, opacity: 0.95, side: THREE.DoubleSide }),
+const images = [
+  'images/soulseek.jpeg',
+  'images/vhf.jpg',
+  'images/vhf2.jpg',
+  'images/cruise.png',
+  'images/onesies.png',
+  'images/avocado.png',
 ];
-var cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials);
-var cube = new THREE.Mesh(geometry, cubeMaterial);
+const geometry = new THREE.CubeGeometry(2, 2, 2);
+const textures = images.map((i) => new THREE.TextureLoader().load(i));
+const cubeMaterials = textures.map((t) => new THREE.MeshBasicMaterial({ map: t, side: THREE.DoubleSide }));
+const cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials);
+const cube = new THREE.Mesh(geometry, cubeMaterial);
 scene.add(cube);
 
 camera.position.z = 5;
 
-var animate = function () {
+const animate = function () {
   requestAnimationFrame(animate);
 
   cube.rotation.x += 0.01;
@@ -31,7 +33,7 @@ var animate = function () {
 };
 
 function onDocumentMouseDown(event) {
-  var vector = new THREE.Vector3(
+  const vector = new THREE.Vector3(
     (event.clientX / window.innerWidth) * 2 - 1,
     -(event.clientY / window.innerHeight) * 2 + 1,
     0.5
@@ -39,17 +41,28 @@ function onDocumentMouseDown(event) {
   vector.unproject(camera);
   raycaster.set(camera.position, vector.sub(camera.position).normalize());
 
-  var intersects = raycaster.intersectObject(cube);
+  const intersects = raycaster.intersectObject(cube);
   if (intersects.length > 0) {
-    var index = Math.floor(intersects[0].faceIndex / 2);
+    const index = Math.floor(intersects[0].faceIndex / 2);
     switch (index) {
       case 0:
+        window.open('https://www.hardwater.live/pathway', '_blank'); // ss
+        break;
       case 1:
+        window.open('https://www.hardwater.live/events/vhf-1', '_blank'); // vhf
+        break;
       case 2:
+        window.open('https://www.hardwater.live/events/vhf-2', '_blank'); // vhf2
+        break;
       case 3:
+        window.open('https://www.hardwater.live/events/current', '_blank'); // cruise
+        break;
       case 4:
+        window.open('https://www.hardwater.live/events/woofers-and-onesies', '_blank'); // onesie
+        break;
       case 5:
-        console.log(index);
+        window.open('https://www.hardwater.live/events/avocado-trance', '_blank'); // avocado
+        break;
     }
   }
 }
